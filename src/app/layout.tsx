514abed4e6be167dcdfdbbd48e7c21b022e95ae3
BD sDirect <dbsdirect.sdm@smartdatainc.net>
2025-02-04 18:20:51 +0530

    Remove HeroSection import from layout and add course images to FeaturedCourses component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import HeroSection from "@/components/HeroSection";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +31,6 @@ export default function RootLayout({
         
         <div className="relative w-full flex items-center justify-center">
         <Navbar />
-        {/* <HeroSection /> */}
         </div>
         {children}
 
--- src/components/FeaturedCourses.tsx
+++ src/components/FeaturedCourses.tsx
@@ -35,6 +35,7 @@ function FeaturedCourses() {
                             <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
                                 <p className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
                                 <p className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">{course.description}</p>
+                                <img src={`courses/${course.slug}.jpg`} alt={course.title} className="w-full h-48 object-cover mt-4 mb-4"/>
                                 <Link href={`/courses/${course.slug}`}>
                                 Learn More
                                 </Link>
